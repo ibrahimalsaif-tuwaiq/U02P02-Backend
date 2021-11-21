1144ceaa50fa8069be9141575213e6cbc50bcd89
ibrahimalsaif-tuwaiq <Ibrahim.Husain.elq.js@tuwaiq.edu.sa>
2021-11-22 01:10:41 +0300

    signup controller

--- routers/controllers/signup.js
+++ routers/controllers/signup.js
@@ -1,5 +1,25 @@
-const signup = (req, res) => {
-  // code
+const usersModel = require("./../../db/models/usersSchema");
+
+const signup = async (req, res) => {
+  const { username, email, password, about } = req.body;
+
+  const userExists = await usersModel.findOne({
+    $or: [{ username: username }, { email: email }],
+  });
+  if (userExists) {
+    return res.status(403).json({
+      error: "Email or Username already taken!",
+    });
+  } else {
+    const user = await new usersModel({
+      username,
+      email,
+      password,
+      about,
+    });
+    await user.save();
+    res.status(200).json({ message: "Signup success! Please Login. " });
+  }
 };
 
 module.exports = signup;
